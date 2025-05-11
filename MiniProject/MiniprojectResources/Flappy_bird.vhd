747c1bf625b92c8da23fa08ad455b8c45ccd653a
Ashwin Singh <162235137+nisa397@users.noreply.github.com>
2025-05-12 02:24:57 +1200

    figured out how to use mouse

--- MiniProject/MiniprojectResources/Flappy_bird.vhd
+++ MiniProject/MiniprojectResources/Flappy_bird.vhd
@@ -5,12 +5,21 @@ use ieee.std_logic_unsigned.all;
 
 entity Flappy_bird is
     port(
+		PS2_CLK		: INOUT std_logic;
+		PS2_DAT		: INOUT std_logic;
       clk_50MHz   : IN  std_logic;  -- DE0-CV clock
       h_sync      : OUT std_logic;  -- VGA horizontal sync
       v_sync      : OUT std_logic;  -- VGA vertical sync
       red         : OUT std_logic;  -- VGA red output
       green       : OUT std_logic;  -- VGA green output
-      blue        : OUT std_logic   -- VGA blue output
+      blue        : OUT std_logic;   -- VGA blue output
+		LEDR0			: OUT std_logic
+	  --HEX0 : out STD_LOGIC_VECTOR(6 downto 0); --sso
+	  --HEX1 : out STD_LOGIC_Vector(6 downto 0); --sst
+	  --HEX2 : out STD_LOGIC_Vector(6 downto 0);
+	  --HEX3 : out STD_LOGIC_VECTOR(6 downto 0); --sso
+	  --HEX4 : out STD_LOGIC_Vector(6 downto 0); --sst
+	  --HEX5 : out STD_LOGIC_Vector(6 downto 0) 
     );
 end entity Flappy_bird;
 
@@ -22,13 +31,15 @@ architecture Behavioral of Flappy_bird is
   
   
   -- Mouse signals
-  signal ps2_reset : std_logic;
-  signal ps2_data : std_logic;
-  signal ps2_click : std_logic;
+  signal ps2_reset : std_logic := '0';
   signal ps2_left: std_logic;
   signal ps2_right: std_logic;
   signal ps2_cursor_row: std_logic_vector(9 DOWNTO 0);
   signal ps2_cursor_col: std_logic_vector(9 DOWNTO 0);
+  
+  
+
+  
   
   --Cursor signals
   signal cursor_on: std_logic;
@@ -72,16 +83,13 @@ architecture Behavioral of Flappy_bird is
 		 mouse_cursor_row 			: OUT std_logic_vector(9 DOWNTO 0); 
 		 mouse_cursor_column 		: OUT std_logic_vector(9 DOWNTO 0));       	
 	end component;
-
-	component cursor_drawer is 
-		port (
-        clk            : in  STD_LOGIC;
-        video_row, video_column  : in  STD_LOGIC_VECTOR (9 downto 0);
-        cursor_row, cursor_column: in  STD_LOGIC_VECTOR (9 downto 0);
-		cursor_on : out STD_LOGIC
-		);
+	
+	component BCD_to_SevenSeg is
+		port (BCD_digit : in std_logic_vector(3 downto 0);
+           SevenSeg_out : out std_logic_vector(6 downto 0));
 	end component;
 
+	
   begin
 
 	
@@ -102,23 +110,59 @@ architecture Behavioral of Flappy_bird is
 	port map(
 	clock_25Mhz => clk_25MHz,
 	reset => ps2_reset,
-	mouse_data => ps2_data,
-	mouse_clk => ps2_click,
+	mouse_data => PS2_DAT,
+	mouse_clk => PS2_CLK,
 	left_button => ps2_left,
 	right_button => ps2_right,
 	mouse_cursor_row => ps2_cursor_row,
 	mouse_cursor_column => ps2_cursor_col
 	);
 	
-	testCursor : cursor_drawer
-	port map(
-	clk => clk_25MHz,
-	video_row => pixel_row,
-	video_column => pixel_column,
-	cursor_row => ps2_cursor_row,
-	cursor_column => ps2_cursor_col,
-	cursor_on => cursor_on
-	);
+	
+	
+	--Col
+	--sso0: BCD_to_SevenSeg
+	--port map(
+	--BCD_digit => ps2_cursor_col(3 downto 0),
+	--SevenSeg_out => HEX0
+	--);
+	
+	--sst0: BCD_to_SevenSeg
+	--port map(
+	--BCD_digit => ps2_cursor_col(7 downto 4),
+	--SevenSeg_out => HEX1
+	--);
+	
+	--ssh0: BCD_to_SevenSeg
+	--port map (
+    --BCD_digit => "00" & ps2_cursor_col(9 downto 8),
+    --SevenSeg_out => HEX2
+	--);
+	
+	--Row
+	--sso1: BCD_to_SevenSeg
+	--port map(
+	--BCD_digit => ps2_cursor_row(3 downto 0),
+	--SevenSeg_out => HEX3
+	--);
+	
+	--sst1: BCD_to_SevenSeg
+	--port map(
+	--BCD_digit => ps2_cursor_row(7 downto 4),
+	--SevenSeg_out => HEX4
+	--);
+	
+	--ssh1: BCD_to_SevenSeg
+	--port map (
+    --BCD_digit => "00" & ps2_cursor_row(9 downto 8),
+    --SevenSeg_out => HEX5
+	--);
+	
+	
+	
+	
+	
+	
 	
 
     -- Instantiate the ball component
@@ -141,10 +185,12 @@ architecture Behavioral of Flappy_bird is
   -- Logic to combine bird and background colors
   -- If the current pixel is part of the bird, use the bird's color.
   -- Otherwise, use a constant background color (e.g., green background).
-  red_pixel   <= '0' when (ball_on = '1') or (cursor_on = '1')else '0'; -- Bird: red, Background: no red
-  green_pixel <= '0' when ball_on = '1' or (cursor_on = '1') else '1'; -- Bird: no green, Background: green
-  blue_pixel  <= '1' when ball_on = '1' or (cursor_on = '1')else '0'; -- Bird: no blue, Background: no blue
+  red_pixel   <= '0' when ball_on = '1' else '0'; -- Bird: red, Background: no red
+  green_pixel <= '0' when ball_on = '1' else '1'; -- Bird: no green, Background: green
+  blue_pixel  <= '1' when ball_on = '1' else '0'; -- Bird: no blue, Background: no blue
   
+  
+  LEDR0 <= ps2_left;
   
   -- Instantiate the VGA sync component
   VGASync: vga_sync
